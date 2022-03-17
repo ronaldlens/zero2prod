c114c9b64afb03fd5e4d943cf07e19737dcdfaab
Ronald Lens <ronald@ronaldlens.com>
2022-03-17 21:40:10 +0100

    health check and firest test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:3000")
+        .expect("Could not bind to 127.0.0.1:3000");
+
+    run(listener)?.await
+
 }
